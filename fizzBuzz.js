756bf847ce570107f54506150189a56e4f9e8adf
Johandro Gonzalez <88999789+JohandroG@users.noreply.github.com>
2021-11-17 16:05:41 -0600

    first commit

---
+++ fizzBuzz.js
@@ -0,0 +1,22 @@
+function fizzBuzz(nummax){
+    for(let i= 1; i <= nummax; i++){
+        if( i % 5 === 0 && i % 3 === 0){
+            console.log("FizzBuzz");
+        }
+        else{
+            if( i % 5 === 0){
+                console.log("Buzz");
+            }
+            else{ 
+                if( i % 3 === 0){
+                    console.log("Fizz")
+                }
+                else {
+                    console.log(i)
+                }
+            }
+        }
+    }
+}
+
+fizzBuzz(20); 
